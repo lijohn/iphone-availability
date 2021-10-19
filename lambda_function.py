be6ed109a8939e88868c33e958053911c55f3042
John Li <li.john97@gmail.com>
2021-10-19 01:14:52 -0400

    simplify url

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import requests
 import boto3
 
 def lambda_handler(event, context):
-    url = 'https://www.apple.com/shop/fulfillment-messages?pl=true&cppart=ATT_IPHONE13PRO&parts.0=MLTT3LL/A&location='
+    url = 'https://www.apple.com/shop/fulfillment-messages?parts.0=MLTT3LL/A&location='
     zipCodes = ['19702', '22043', '07078', '10003']
     availableStores = []
     message = 'iPhone not available'
@@ -14,8 +14,7 @@ def lambda_handler(event, context):
         for store in stores:
             availability = store['partsAvailability']['MLTT3LL/A']
             if availability['storeSelectionEnabled'] or availability['pickupDisplay'] == 'available':
-                storeName = store['storeName']
-                availableStores.append(storeName)
+                availableStores.append(store['storeName'])
                 break
 
     if len(availableStores) > 0:
