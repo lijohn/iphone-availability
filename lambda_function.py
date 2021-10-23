31647ac4979881a336ab3a76eba8df59dfc37eb0
John Li <li.john97@gmail.com>
2021-10-23 15:28:11 -0400

    write individual store stock to dynamo

--- lambda_function.py
+++ lambda_function.py
@@ -1,60 +1,59 @@
 import json
 import requests
-from time import time
+import time
 import boto3
 
 def lambda_handler(event, context):
     url = 'https://www.apple.com/shop/fulfillment-messages?parts.0=MLTT3LL/A&location='
     zipCodes = ['19702', '22043', '07078']
-    availableStores = set()
+    evaluatedStores, availableStores = set(), set()
     message = 'iPhone not available'
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('iphone-availability')
+    currentTime = int(time.time())
 
     for zipCode in zipCodes:
         response = requests.get(url + zipCode).json()
         stores = response['body']['content']['pickupMessage']['stores']
         for store in stores:
-            availability = store['partsAvailability']['MLTT3LL/A']
-            if availability['storeSelectionEnabled'] or availability['pickupDisplay'] == 'available':
-                availableStores.add(store['storeName'])
-                break
+            storeName = store['storeName']
+            if storeName not in evaluatedStores and is_available(store):
+                storeUpdate = get_store_update(table, storeName)
+                if storeUpdate:
+                    lastMessage = storeUpdate['last_message']
+                    if currentTime > lastMessage + 600:
+                        availableStores.add(storeName)
+                        update_store(table, storeName, currentTime)
+                    else:
+                        update_store(table, storeName, lastMessage)
+                else:
+                    availableStores.add(storeName)
+                    update_store(table, storeName, currentTime)
+            evaluatedStores.add(storeName)
 
     if len(availableStores) > 0:
-        message = 'iPhone available at ' + ', '.join(availableStores)
-
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('iphone-availability')
-        lastMessage = get_last_message(table)
-
-        currentTime = int(time())
-        count = lastMessage['count']
-        lastUpdate = lastMessage['timestamp']
-
-        # only send repeated messages every 10 minutes
-        if lastMessage['message'] == message:
-            if currentTime > lastUpdate + 600:
-                send_message(message + " (" + str(count) + " minutes)")
-                lastUpdate = currentTime
-            update_last_message(table, message, lastUpdate, count + 1)
-        else:
-            send_message(message)
-            update_last_message(table, message, currentTime, 0)
+        message = 'iPhone available at ' + ', '.join(sorted(availableStores))
+        send_message(message)
 
     return {
         'statusCode': 200,
         'body': json.dumps(message)
     }
 
-def get_last_message(table):
-    item = table.get_item(Key={'key': 'lastMessage'})
-    return item['Item']
+def is_available(store):
+    availability = store['partsAvailability']['MLTT3LL/A']
+    return availability['storeSelectionEnabled'] or availability['pickupDisplay'] == 'available'
 
-def update_last_message(table, message, currentTime, count):
+def get_store_update(table, store):
+    item = table.get_item(Key={'key': store})
+    return item.get('Item')
+
+def update_store(table, store, lastMessage):
     table.put_item(
         Item={
-            'key': 'lastMessage',
-            'message': message,
-            'timestamp': currentTime,
-            'count': count
+            'key': store,
+            'last_message': lastMessage,
+            'last_update': time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime())
         }
     )
 
