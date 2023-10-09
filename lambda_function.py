1d9a9e58db8b14313c034aff5256bab12856ec9a
John Li <li.john97@gmail.com>
2023-10-08 23:01:26 -0700

    add multiple part compatibility

--- lambda_function.py
+++ lambda_function.py
@@ -2,62 +2,83 @@ import json
 import requests
 import time
 import boto3
+import os
 
-part = 'MQ003LL/A'
+parts = ['MTQN3LL/A', 'MTQQ3LL/A', 'MU673LL/A']
 locations = ['Portland, OR']
 states = ['OR']
 
+DEFAULT_MESSAGE = 'No products available or cooldown has not passed'
+COOLDOWN_PERIOD = 900  # time between notifications from the same store
+
 def lambda_handler(event, context):
-    url = 'https://www.apple.com/shop/fulfillment-messages?parts.0={}&location='.format(part)
-    evaluatedStores = set()  # can be initialized here as a deny list
-    availableStores = set()
-    message = 'iPhone not available'
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('iphone-availability')
-    currentTime = int(time.time())
-    cooldown = 900  # time between notifications from the same store
-
-    for location in locations:
-        response = requests.get(url + location).json()
-        stores = response['body']['content']['pickupMessage']['stores']
-        for store in stores:
-            storeName = store['storeName']
-            if storeName not in evaluatedStores and is_available(store):
-                storeUpdate = get_store_update(table, storeName)
-                if storeUpdate:
-                    lastMessage = storeUpdate['last_message']
-                    count = storeUpdate['count']
-                    if currentTime > lastMessage + cooldown:
-                        availableStores.add(storeName)
-                        update_store(table, storeName, currentTime, count + 1)
-                    else:
-                        update_store(table, storeName, lastMessage, count + 1)
-                else:
-                    availableStores.add(storeName)
-                    update_store(table, storeName, currentTime, 1)
-            evaluatedStores.add(storeName)
-
-    if len(availableStores) > 0:
-        message = 'iPhone available at ' + ', '.join(sorted(availableStores))
-        send_message(message)
+    for part in parts:
+        message = check_store_availability(part)
+        if message != DEFAULT_MESSAGE:
+            send_message(message, part)
 
     return {
         'statusCode': 200,
         'body': json.dumps(message)
     }
 
-def is_available(store):
+def check_store_availability(part):
+    url = 'https://www.apple.com/shop/fulfillment-messages?parts.0={}&location='.format(part)
+    evaluatedStores = set()  # can be initialized here as a deny list
+    availableStores = set()
+    message = DEFAULT_MESSAGE
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['DYNAMO_TABLE_NAME'])
+    currentTime = int(time.time())
+    partName = part
+
+    for location in locations:
+        response = requests.get(url + location).json()
+        stores = response['body']['content']['pickupMessage']['stores']
+        for store in stores:
+            storeName = store['storeName']
+            if storeName not in evaluatedStores and is_available(store, part):
+                partName = get_part_name(store, part)
+                storeUpdate = get_store_update(table, storeName, part)
+                if storeUpdate:
+                    lastMessage = storeUpdate['last_message']
+                    count = storeUpdate['count']
+                    if currentTime > lastMessage + COOLDOWN_PERIOD:
+                        availableStores.add(storeName)
+                        update_store(table, storeName, part, currentTime, count + 1)
+                    else:
+                        update_store(table, storeName, part, lastMessage, count + 1)
+                else:
+                    availableStores.add(storeName)
+                    update_store(table, storeName, part, currentTime, 1)
+            evaluatedStores.add(storeName)
+
+    if len(availableStores) > 0:
+        message = partName + ' available at ' + ', '.join(sorted(availableStores))
+
+    return message
+
+def is_available(store, part):
     availability = store['partsAvailability'][part]
     return store['state'] in states and availability['pickupDisplay'] == 'available'
 
-def get_store_update(table, store):
-    item = table.get_item(Key={'key': store})
+def get_part_name(store, part):
+    return store['partsAvailability'][part]['messageTypes']['regular']['storePickupProductTitle']
+
+def get_store_update(table, store, part):
+    item = table.get_item(
+        Key={
+            'store': store,
+            'part': part
+        }
+    )
     return item.get('Item')
 
-def update_store(table, store, lastMessage, count):
+def update_store(table, store, part, lastMessage, count):
     table.put_item(
         Item={
-            'key': store,
+            'store': store,
+            'part': part,
             'last_message': lastMessage,
             'last_message_strft': format_time(lastMessage),
             'last_update': format_time(),
@@ -68,9 +89,15 @@ def update_store(table, store, lastMessage, count):
 def format_time(epochTime=None):
     return time.strftime('%Y-%m-%d %H:%M:%S %Z', time.localtime(epochTime))
 
-def send_message(message):
+def send_message(message, part):
     client = boto3.client('sns')
     client.publish(
-        TopicArn='arn:aws:sns:us-east-1:233229421924:iphone-availability',
-        Message=message
+        TopicArn=os.environ['SNS_ARN'],
+        Message=message,
+        MessageAttributes={
+            'model': {
+                'DataType': 'String',
+                'StringValue': part
+          }
+        }
     )
